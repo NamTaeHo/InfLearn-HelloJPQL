fe703f13899bc822b68aa2eab8c14e132ede6077
NamTaeHo <han127kr@gmail.com>
2024-01-16 20:44:45 +0900

    패치조인

--- src/main/java/jpql/JpqlMain.java
+++ src/main/java/jpql/JpqlMain.java
@@ -20,30 +20,57 @@ public class JpqlMain {
         try {
 
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
 
-            Member member = new Member();
-            member.setUsername("관리자");
-            member.setAge(10);
-            member.setTeam(team);
-            member.setMemberType(MemberType.ADMIN);
+            em.persist(teamA);
 
+            Team teamB = new Team();
+            teamB.setName("팀b");
+            em.persist(teamB);
 
-            em.persist(member);
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setAge(10);
+            member1.setTeam(teamA);
+            member1.setMemberType(MemberType.USER);
+
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setAge(18);
+            member2.setTeam(teamA);
+            member2.setMemberType(MemberType.USER);
+
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setAge(11);
+            member3.setTeam(teamB);
+            member3.setMemberType(MemberType.USER);
+
+            em.persist(member3);
 
 
             em.flush();
             em.clear();
 
 
-            String query = "select t.members From Team t";
+            String query = "select distinct t from Team t join fetch t.members";
 
-            Collection resultList = em.createQuery(query, Collection.class).getResultList();
-            for (Object o : resultList) {
-                System.out.println("o = " + o);
-                
+            List<Team> resultList = em.createQuery(query, Team.class).getResultList();
+            for (Team team : resultList) {
+                System.out.println("Team.getName() = " + team.getName()+" members  = " + team.getMembers().size());
+                for(Member member : team.getMembers()){
+                    System.out.println("member = " + member);
+                }
+
+
             }
 
 
